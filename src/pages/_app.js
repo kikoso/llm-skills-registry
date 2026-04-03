fc69f5c984ef6b3dcc383a7814019ccd53bf8943
Enrique López-Mañas <eenriquelopez@gmail.com>
2026-04-03 21:49:44 +0200

    Remove Google Analytics from the project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,33 +1,7 @@
 import '../styles/globals.css';
-import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-M37D0QHKMX`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-M37D0QHKMX', {
-              page_path: window.location.pathname,
-              site_id: 'findskills',
-              send_page_view: true
-            });
-          `,
-        }}
-      />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
