6ceae65b3a788975fc2c3fb3165bd721f95ac29c
Enrique López-Mañas <eenriquelopez@gmail.com>
2026-04-04 11:13:55 +0200

    Add Firebase Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,36 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        id="firebase-analytics"
+        type="module"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+            import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+
+            const firebaseConfig = {
+              apiKey: "AIzaSyDCt_H4sxMOav_GwzkbWW_rQyczjl19-B8",
+              authDomain: "find-skills.firebaseapp.com",
+              projectId: "find-skills",
+              storageBucket: "find-skills.firebasestorage.app",
+              messagingSenderId: "538719779408",
+              appId: "1:538719779408:web:1e33469820ae1ecc9b6f55",
+              measurementId: "G-2H1RS0BZ3H"
+            };
+
+            const app = initializeApp(firebaseConfig);
+            const analytics = getAnalytics(app);
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
